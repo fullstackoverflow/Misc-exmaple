13ff9977eef8c55e31454d3d857e0968ab15d28f
Fang Yi <yifang@faceunity.com>
2019-08-06 18:35:12 +0800

    update 3.0

--- src/__tests__/router.spec.ts
+++ src/__tests__/router.spec.ts
@@ -1,12 +1,13 @@
+import 'reflect-metadata';
 import request from 'supertest';
 import { Server } from '../app';
-import { Expect, TestFixture, AsyncTest, SetupFixture } from 'alsatian';
+import { Expect, TestFixture, AsyncTest, SetupFixture, Test } from 'alsatian';
 
 @TestFixture('test')
 export class ExampleTestFixture {
 	instance: request.SuperTest<request.Test>;
 
-	@AsyncTest('test')
+	@Test('test')
 	public async test1() {
 		const instance = request(Server);
 		const response = await instance.get('/?test=111');
