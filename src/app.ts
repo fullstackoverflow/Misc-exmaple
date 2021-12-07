f6cd8db969f48c08ac4e5695d7bf9c177430d9a1
Fang Yi <yifang@faceunity.com>
2021-12-07 10:52:26 +0800

    deps:更新@tosee/misc依赖至5.0.2

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import { ReqLog } from './middlewares/reqLog';
 import { Misc, logger } from '@tosee/misc';
 import { errorHandler } from './middlewares/errorHandler';
 import { Config } from '@tosee/config';
+import { resolve } from 'path';
 
 const app = new Misc({
 	keys: ['web_platform'],
 	beforeall: [logger.Middleware(), errorHandler(), ReqLog()],
+	router: resolve(__dirname, './router/**/*.*{ts,js}'),
 	protocol: Config.instance.protocol,
 	port: Config.instance.port,
 });
--- src/router/exmaple.ts
+++ src/router/exmaple.ts
@@ -1,6 +1,6 @@
 import Koa from 'koa';
 import { Service } from '../service/Service';
-import { Controller, GET, POST, Validate } from '@tosee/misc';
+import { Controller, GET, POST, Validate, Body } from '@tosee/misc';
 import { Autowired } from '@tosee/util';
 import { Value } from '@tosee/config';
 import { File } from '@tosee/busboy';
@@ -20,13 +20,13 @@ export default class Router {
 		stream.resume();
 		return filename;
 	})
-	async test(ctx: Koa.Context) {
-		ctx.body = ctx.request.body;
+	async test(@Body body:any) {
+		return body;
 	}
 
 	@POST('/test2')
 	@Validate({ schema: Test })
-	async test2(ctx: Koa.Context) {
-		ctx.body = ctx.request.body as Test;
+	async test2(@Body body:Test) {
+		return body;
 	}
 }
